0f73dc1fb65c4ad7243fda3abd786b679e580867
Volodymyr <fedunivvolodymyr@gmail.com>
2023-06-20 14:30:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://willowy-centaur-da4a04.netlify.app/">
+          Claire Pasquiou
+        </a>{" "}
+        and is
+        <a
+          rel="noreferrer"
+          href="https://github.com/ClairePasquiou/react-weather-app-final"
+          target="_blank">
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
